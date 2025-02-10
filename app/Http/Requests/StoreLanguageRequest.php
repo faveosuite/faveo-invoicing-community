5ac8f6e20574dd179ec5b1b382acc02bd88d12d1
Perumal408 <perumal.s@ladybirdweb.com>
2025-02-10 16:00:34 +0530

    Fixes #3191

---
+++ app/Http/Requests/StoreLanguageRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\File;
+
+class StoreLanguageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'language' => ['required', function ($attribute, $value, $fail) {
+                $availableLanguages = array_map('basename', File::directories(lang_path()));
+                if (! in_array($value, $availableLanguages)) {
+                    return $fail(__('lang.invalid_language'));
+                }
+            }],
+        ];
+    }
+}
