59f5c7b5ed386268ce728aab19905387fc5cff2a
SanthanuKC <santhanu.kc@ladybirdweb.com>
2025-03-06 14:48:14 +0530

    initialChanges
    initialFixes
    
    phaseTwo
    
    initialFixes
    
    seconFixes
    
    langFixes
    
    sweetFixes
    
    deleteuser
    
    stateFixes
    
    squashFixes

--- resources/views/themes/default1/front/page/index.blade.php
+++ resources/views/themes/default1/front/page/index.blade.php
@@ -122,7 +122,8 @@ All Pages
 @stop
 
 @section('icheck')
-<script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
     function checking(e){
           
           $('#pages-table').find("td input[type='checkbox']").prop('checked', $(e).prop('checked'));
@@ -131,32 +132,59 @@ All Pages
 
      $(document).on('click','#bulk_delete',function(){
       var id=[];
-      if (confirm("Are you sure you want to delete this?"))
-        {
-            $('.page_checkbox:checked').each(function(){
-              id.push($(this).val())
-            });
-            if(id.length >0)
-            {
-               $.ajax({
-                      url:"{!! route('pages-delete') !!}",
-                      method:"delete",
-                      data: $('#check:checked').serialize(),
-                      beforeSend: function () {
-                $('#gif').show();
-                },
-                success: function (data) {
-                $('#gif').hide();
-                $('#response').html(data);
-                location.reload();
-                }
-               })
-            }
-            else
-            {
-                alert("Please select at least one checkbox");
-            }
-        }  
+
+         var swl=swal.fire({
+             title:"<h2 style='text-align: left; padding-left: 17px !important; margin-bottom:10px !important;'>{{Lang::get('message.Delete')}}</h2>",
+             html: "<div  style='display: flex; flex-direction: column; align-items:stretch; width:100%; margin:0px !important'>" +
+                 "<div style='border-top: 1px solid #ccc; border-bottom: 1px solid #ccc;padding-top: 13px;'>" +
+                 "<p style='text-align: left; margin-left:17px'>{{Lang::get('message.page_delete')}}</p>"+"</div>" +
+                 "</div>",
+             showCancelButton: true,
+             showCloseButton: true,
+             position:"top",
+             width:"600px",
+             confirmButtonText: @json(trans('message.Delete')),
+             confirmButtonColor: "#007bff",
+         }).then((result)=> {
+             if (result.isConfirmed) {
+                 $('.page_checkbox:checked').each(function(){
+                     id.push($(this).val())
+                 });
+                 if(id.length >0)
+                 {
+                     $.ajax({
+                         url:"{!! route('pages-delete') !!}",
+                         method:"delete",
+                         data: $('#check:checked').serialize(),
+                         beforeSend: function () {
+                             $('#gif').show();
+                         },
+                         success: function (data) {
+                             $('#gif').hide();
+                             $('#response').html(data);
+                             location.reload();
+                         }
+                     })
+                 }
+
+                 else {
+                     swal.fire({
+                         title:"<h2 style='text-align: left; padding-left: 17px !important; margin-bottom:10px !important;'>{{Lang::get('message.Select')}}</h2>",
+                         html: "<div  style='display: flex; flex-direction: column; align-items:stretch; width:100%; margin:0px !important'>" +
+                             "<div style='border-top: 1px solid #ccc; border-bottom: 1px solid #ccc;padding-top: 13px;'>" +
+                             "<p style='text-align: left; margin-left:17px'>{{Lang::get('message.sweet_page')}}</p>"+"</div>" +
+                             "</div>",
+                         position: 'top',
+                         confirmButtonText: "OK",
+                         showCloseButton: true,
+                         confirmButtonColor: "#007bff",
+                         width:"600px",
+                     })
+                 }
+             }else if (result.dismiss === Swal.DismissReason.cancel) {
+                 window.close();
+             }
+         })
 
      });
 </script>
--- resources/views/themes/default1/product/group/index.blade.php
+++ resources/views/themes/default1/product/group/index.blade.php
@@ -118,6 +118,7 @@ Groups
     @stop
 
 @section('icheck')
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
   <script>
     function checking(e){
@@ -128,32 +129,58 @@ Groups
 
      $(document).on('click','#bulk_delete',function(){
       var id=[];
-      if (confirm("Are you sure you want to delete this?"))
-        {
-            $('.group_checkbox:checked').each(function(){
-              id.push($(this).val())
-            });
-            if(id.length >0)
-            {
-               $.ajax({
-                      url:"{!! route('groups-delete') !!}",
-                      method:"delete",
-                      data: $('#check:checked').serialize(),
-                      beforeSend: function () {
-                $('#gif').show();
-                },
-                success: function (data) {
-                $('#gif').hide();
-                $('#response').html(data);
-                location.reload();
-                }
-               })
-            }
-            else
-            {
-                alert("Please select at least one checkbox");
-            }
-        }  
+
+         var swl=swal.fire({
+             title:"<h2 style='text-align: left; padding-left: 17px !important; margin-bottom:10px !important;'>{{Lang::get('message.Delete')}}</h2>",
+             html: "<div  style='display: flex; flex-direction: column; align-items:stretch; width:100%; margin:0px !important'>" +
+                 "<div style='border-top: 1px solid #ccc; border-bottom: 1px solid #ccc;padding-top: 13px;'>" +
+                 "<p style='text-align: left; margin-left:17px'>{{Lang::get('message.group_delete')}}</p>"+"</div>" +
+                 "</div>",
+             showCancelButton: true,
+             showCloseButton: true,
+             position:"top",
+             width:"600px",
+             confirmButtonText: @json(trans('message.Delete')),
+             confirmButtonColor: "#007bff",
+         }).then((result)=> {
+             if (result.isConfirmed) {
+                 $('.group_checkbox:checked').each(function(){
+                     id.push($(this).val())
+                 });
+                 if(id.length >0)
+                 {
+                     $.ajax({
+                         url:"{!! route('groups-delete') !!}",
+                         method:"delete",
+                         data: $('#check:checked').serialize(),
+                         beforeSend: function () {
+                             $('#gif').show();
+                         },
+                         success: function (data) {
+                             $('#gif').hide();
+                             $('#response').html(data);
+                             location.reload();
+                         }
+                     })
+                 }
+                 else {
+                     swal.fire({
+                         title:"<h2 style='text-align: left; padding-left: 17px !important; margin-bottom:10px !important;'>{{Lang::get('message.Select')}}</h2>",
+                         html: "<div  style='display: flex; flex-direction: column; align-items:stretch; width:100%; margin:0px !important'>" +
+                             "<div style='border-top: 1px solid #ccc; border-bottom: 1px solid #ccc;padding-top: 13px;'>" +
+                             "<p style='text-align: left; margin-left:17px'>{{Lang::get('message.sweet_group')}}</p>"+"</div>" +
+                             "</div>",
+                         position: 'top',
+                         confirmButtonText: "OK",
+                         showCloseButton: true,
+                         confirmButtonColor: "#007bff",
+                         width:"600px",
+                     })
+                 }
+             }else if (result.dismiss === Swal.DismissReason.cancel) {
+                 window.close();
+             }
+         })
 
      });
 
