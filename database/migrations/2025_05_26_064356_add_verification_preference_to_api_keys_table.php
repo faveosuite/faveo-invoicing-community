481b625999e05ae942d1c828e41a2e7c5e377d28
sadhasivam-ladybirdweb <sadhasivam.sekar@ladybirdweb.com>
2025-05-27 16:42:20 +0530

    verification update

---
+++ database/migrations/2025_05_26_064356_add_verification_preference_to_api_keys_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('api_keys', function (Blueprint $table) {
+            $table->string('verification_preference')->nullable()->default('email')->after('msg91_third_party_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('api_keys', function (Blueprint $table) {
+            $table->dropColumn('verification_preference');
+        });
+    }
+};
