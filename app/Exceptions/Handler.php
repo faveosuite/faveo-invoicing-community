2a05b9db68ba0196d0260da380a3bac25fd66af5
SanthanuKC <santhanu.kc@ladybirdweb.com>
2025-07-04 19:52:04 +0530

    Final

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -33,7 +33,7 @@ class Handler extends ExceptionHandler
      * @return void
      */
     public function report(Throwable $exception)
-    {
+    {dd($exception);
         // Check if the exception is an UnauthenticatedException
         if (! $exception instanceof AuthenticationException) {
             // Send unhandled exceptions to Bugsnag
