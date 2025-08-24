8bdce1a7fe8946e822414881b29f4766e9f46c99
Perumal408 <perumal.s@ladybirdweb.com>
2025-08-25 00:58:54 +0530

    jen_dusk_bill_1

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -29,7 +29,7 @@ abstract class DuskTestCase extends BaseTestCase
     {
         $options = (new ChromeOptions)->addArguments(collect([
             $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
-            '--disable-search-engine-choice-screen',
+            '--disable-search-engine-choice-screen','--no-sandbox',
         ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
             return $items->merge([
                 '--disable-gpu',
