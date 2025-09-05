e9a897a23d30639f9e483852a2bb7634320815d7
sadhasivam-ladybirdweb <sadhasivam.sekar@ladybirdweb.com>
2025-09-05 15:44:33 +0530

    add seeder

--- database/seeders/v4_0_2_4/DatabaseSeeder.php
+++ database/seeders/v4_0_2_4/DatabaseSeeder.php
@@ -2,22 +2,7 @@
 
 namespace Database\Seeders\v4_0_2_4;
 
-use App\ApiKey;
-use App\Model\Common\EmailMobileValidationProviders;
 use App\Model\Common\ManagerSetting;
-use App\Model\Common\Msg91Status;
-use App\Http\Controllers\Common\PipedriveController;
-use App\Model\Common\PipedriveGroups;
-use App\Model\Common\PipedriveLocalFields;
-use App\Model\Common\PricingTemplate;
-use App\Model\Github\Github;
-use App\Model\Mailjob\ExpiryMailDay;
-use App\Model\Order\InvoiceItem;
-use App\Model\Order\Order;
-use App\Model\Product\Product;
-use App\Model\Product\Subscription;
-use App\Plugins\Recaptcha\Model\RecaptchaSetting;
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -28,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->addMangerRole();
-        $this->createRecaptcha();
     }
 
     public function addMangerRole(): void
@@ -39,10 +23,5 @@ class DatabaseSeeder extends Seeder
                 ['auto_assign' => 0]
             );
         }
-    }
-
-    public function createRecaptcha()
-    {
-        RecaptchaSetting::firstOrCreate([]);
     }
 }
