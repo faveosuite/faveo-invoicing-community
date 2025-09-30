de16bb4e2a017f2f5cce2739e0d5776cc87d047c
SanthanuKC <santhanu.kc@ladybirdweb.com>
2025-09-30 10:57:19 +0530

    start4

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -37,8 +37,8 @@ class ProfileController extends Controller
             $state = getStateByCode($user->state);
             $states = findStateByRegionId($user->country);
             $bussinesses = \App\Model\Common\Bussiness::pluck('name', 'short')->toArray();
-
-            return view('themes.default1.user.profile', compact('bussinesses', 'user', 'timezones', 'state', 'states', 'is2faEnabled', 'dateSinceEnabled'));
+            return successResponse('',['bussinesses'=>$bussinesses, 'user'=>$user, 'timezones'=>$timezones, 'state'=>$state, 'states'=>$states, 'is2faEnabled'=>$is2faEnabled, 'dateSinceEnabled'=>$dateSinceEnabled]);
+//            return view('themes.default1.user.profile', compact('bussinesses', 'user', 'timezones', 'state', 'states', 'is2faEnabled', 'dateSinceEnabled'));
         } catch (\Exception $e) {
             return redirect()->back()->with('fails', $e->getMessage());
         }
@@ -53,10 +53,11 @@ class ProfileController extends Controller
                 $user->profile_pic = basename($path);
             }
             $user->fill($request->input())->save();
-
+//            return successResponse(\Lang::get('message.updated-successfully'));
             return redirect()->back()->with('success', \Lang::get('message.updated-successfully'));
         } catch (\Exception $e) {
             return redirect()->back()->with('fails', $e->getMessage());
+//            return errorResponse($e->getMessage());
         }
     }
 
