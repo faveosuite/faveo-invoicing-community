ad1eee29a3c2f306a272b4c085b164c7464948ff
SanthanuKC <santhanu.kc@ladybirdweb.com>
2025-11-05 13:52:20 +0530

    TestFix

--- tests/Unit/Admin/Payment/PromotionControllerTest.php
+++ tests/Unit/Admin/Payment/PromotionControllerTest.php
@@ -157,6 +157,7 @@ class PromotionControllerTest extends DBTestCase
             'price' => $planPrice->add_price,
             'quantity' => 1,
             'attributes' => [],
+            'associatedModel' => $product,
         ]);
         $promotion = $this->classObject->checkCode('FAVEOCOUPON');
         foreach (\Cart::getContent() as $cart) {
