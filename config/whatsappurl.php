ab0a55a8e3c65e492e3d0555d99c8424e7660a94
sagargaikwad00752 <sagar.gaikwad@ladybirdweb.com>
2025-11-07 19:19:21 +0530

    comp

---
+++ config/whatsappurl.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return[
+
+    'base_url'=>'https://graph.facebook.com',
+    'api_version'=>'v21.0',
+    'endpoints'=>['access_token'=>'oauth/access_token',
+                    'deregister'=>'deregister'],
+
+];
