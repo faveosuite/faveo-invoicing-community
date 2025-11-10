61b29aa8b5460f3bc54bcc20380bc4b8a9ecdba0
SanthanuKC <santhanu.kc@ladybirdweb.com>
2025-11-10 17:59:45 +0530

    yer

--- resources/views/themes/default1/product/product/edit.blade.php
+++ resources/views/themes/default1/product/product/edit.blade.php
@@ -1051,7 +1051,7 @@ tinymce.init({
                 plugins: [
                     'advlist autolink lists link image charmap print preview hr anchor pagebreak wordcount',
                     'searchreplace wordcount visualblocks visualchars code fullscreen',
-                    'insertdatetime media nonbreaking save table contextmenu directionality',
+                    'insertdatetime media nonbreaking save contextmenu directionality',
                     'emoticons template paste textcolor colorpicker textpattern imagetools'
                 ],
                 toolbar1: 'bold italic | wordcount',
