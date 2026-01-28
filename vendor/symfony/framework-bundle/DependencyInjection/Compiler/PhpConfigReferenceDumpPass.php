015832d9ab69e625a6dac8aedec9fd3b346cbc2c
sadhasivam-ladybirdweb <sadhasivam.sekar@faveohelpdesk.com>
2026-01-28 16:29:37 +0530

    update some latest updates

--- vendor/symfony/framework-bundle/DependencyInjection/Compiler/PhpConfigReferenceDumpPass.php
+++ vendor/symfony/framework-bundle/DependencyInjection/Compiler/PhpConfigReferenceDumpPass.php
@@ -11,14 +11,18 @@
 
 namespace Symfony\Bundle\FrameworkBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\Config\Definition\ArrayNode;
 use Symfony\Component\Config\Definition\ArrayShapeGenerator;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\PrototypedArrayNode;
+use Symfony\Component\Config\Loader\ParamConfigurator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ConfigurationExtensionInterface;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\AppReference;
+use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\Routing\Loader\Configurator\RoutesReference;
 
@@ -115,9 +119,13 @@ class PhpConfigReferenceDumpPass implements CompilerPassInterface
             if (!$configuration = $this->getConfiguration($extension, $container)) {
                 continue;
             }
+            $tree = $configuration->getConfigTreeBuilder()->buildTree();
+            if ($tree instanceof ArrayNode && !$tree instanceof PrototypedArrayNode && !$tree->getChildren()) {
+                continue;
+            }
             $anyEnvExtensions[$extensionAlias] = $extension;
             $type = $this->camelCase($extensionAlias).'Config';
-            $appTypes .= \sprintf("\n * @psalm-type %s = %s", $type, ArrayShapeGenerator::generate($configuration->getConfigTreeBuilder()->buildTree()));
+            $appTypes .= \sprintf("\n * @psalm-type %s = %s", $type, ArrayShapeGenerator::generate($tree));
 
             foreach ($knownEnvs as $env) {
                 if ($envs[$env] ?? $envs['all'] ?? false) {
@@ -131,9 +139,13 @@ class PhpConfigReferenceDumpPass implements CompilerPassInterface
             if (!$configuration = $this->getConfiguration($extension, $container)) {
                 continue;
             }
+            $tree = $configuration->getConfigTreeBuilder()->buildTree();
+            if ($tree instanceof ArrayNode && !$tree instanceof PrototypedArrayNode && !$tree->getChildren()) {
+                continue;
+            }
             $anyEnvExtensions[$alias] = $extension;
             $type = $this->camelCase($alias).'Config';
-            $appTypes .= \sprintf("\n * @psalm-type %s = %s", $type, ArrayShapeGenerator::generate($configuration->getConfigTreeBuilder()->buildTree()));
+            $appTypes .= \sprintf("\n * @psalm-type %s = %s", $type, ArrayShapeGenerator::generate($tree));
         }
         krsort($extensionsPerEnv);
 
@@ -170,6 +182,12 @@ class PhpConfigReferenceDumpPass implements CompilerPassInterface
         if ('' !== $routesTypes) {
             $routesTypes = strtr(self::ROUTES_TYPES_TEMPLATE, ['{SHAPE}' => $routesTypes]);
             $routesTypes = substr_replace($phpdoc, $routesTypes, $i);
+        }
+
+        $appTypes = str_replace('\\'.ParamConfigurator::class, 'Param', $appTypes);
+
+        if (!class_exists(Expression::class)) {
+            $appTypes = str_replace('|ExpressionConfigurator', '', $appTypes);
         }
 
         $configReference = strtr(self::REFERENCE_TEMPLATE, [
