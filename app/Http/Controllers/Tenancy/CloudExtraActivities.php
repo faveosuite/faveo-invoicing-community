05ca4cbebbc85ab96e4e120b7e1a2e53dcb94a24
SanthanuKC <santhanu.kc@faveohelpdesk.com>
2026-02-25 14:49:03 +0530

    Final

--- app/Http/Controllers/Tenancy/CloudExtraActivities.php
+++ app/Http/Controllers/Tenancy/CloudExtraActivities.php
@@ -237,7 +237,7 @@ class CloudExtraActivities extends Controller
             if ($request->agentAction == 'decrease' && $oldAgents <= $newAgents) {
                 errorResponse('Invalid operation: The number of agents cannot be reduced below the current allocation.');
             }
-            $totalAgents = $request->agentAction == 'increase' ? $oldAgents + $newAgents : $newAgents - $oldAgents;
+            $totalAgents = $request->agentAction == 'increase' ? $oldAgents + $newAgents : $oldAgents-$newAgents;
             if ($order->client != \Auth::user()->id) {
                 return errorResponse(trans('message.invalid_user'));
             }
@@ -253,8 +253,8 @@ class CloudExtraActivities extends Controller
 
 //            $oldLicense = Order::where('id', $orderId)->latest()->value('serial_key');
             $oldLicense = $order->serial_key;
-            $items = $this->getThePaymentCalculation($newAgents, $oldLicense, $orderId, $request->agentAction);
-            $invoice = (new RenewController())->renewBySubId($request->subId, $items['planId'], '', $items['price'], '', false, $newAgents);
+            $items = $this->getThePaymentCalculation($newAgents, $oldLicense, $orderId,null,$request->agentAction);
+            $invoice = (new RenewController())->renewBySubId($request->subId, $items['planId'], '', $items['price'], '', false, $totalAgents);
 
             if ($invoice) {
                 \Session::put('AgentAlteration', $request->subId);
