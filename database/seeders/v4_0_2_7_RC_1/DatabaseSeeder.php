a6f13c130d125e5a8997e9538e714f47589b3c39
sadhasivam-ladybirdweb <sadhasivam.sekar@faveohelpdesk.com>
2026-04-22 17:52:33 +0530

    removed some files

---
+++ database/seeders/v4_0_2_7_RC_1/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders\v4_0_2_7_RC_1;
+
+use App\License\Database\Seeders\LicenseModuleSeeder;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->licenseModuleSeeder();
+    }
+
+    private function licenseModuleSeeder(): void
+    {
+        $this->call([LicenseModuleSeeder::class]);
+        $this->command->info('Seeded License Module!');
+    }
+
+
+}
